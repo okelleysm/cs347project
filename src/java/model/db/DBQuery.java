1834d0679b95f3fff5855fbd49972703e09cde8b
Shaun <Shaun@Shaun-PC>
2014-11-11 22:02:27 -0500

    Added sqllite into the project, haven't tested

---
+++ src/java/model/db/DBQuery.java
@@ -0,0 +1,41 @@
+package model.db;
+
+import java.sql.*;
+import java.util.*;
+
+/**
+ * This class executes SQL queries.
+ * 
+ * @author R.Grove
+ * @version 2014-10-02
+ */
+public class DBQuery extends DBAccess {
+
+  /**
+   * Execute an SQL query and return the results. The name and
+   * number of each class are concatenated and returned as a String.
+   * 
+   * @param query The query to be executed
+   * @return An ArrayList containing the query results
+   */
+  public ArrayList<String> doQuery(String query) {
+    ArrayList<String> results = new ArrayList<String>();
+    try {
+      Connection connection = getConnection();
+      if (connection == null) {
+        return null;
+      }
+      Statement st = connection.createStatement();
+      ResultSet rs = st.executeQuery(query);
+      while (rs.next()) {
+        results.add(rs.getString(1) + " " + rs.getString(2));
+      }
+      connection.close();
+    } catch (SQLException s) {
+      s.printStackTrace();
+      return null;
+    }
+    return results;
+  }
+
+} // end class
