03afa107b81cba2dc7d776350926199972227320
terrelkl <terrelkl@L14342>
2014-12-01 18:06:31 -0500

    Added Controller Classes, Skeletons

---
+++ src/java/action/ModifyAction.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ *
+ * @author terrelkl
+ */
+public class ModifyAction extends ActionSupport {
+    
+    public ModifyAction() {
+    }
+    
+    public String execute() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
